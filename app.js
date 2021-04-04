3a8c80f4119a9a65203b8650ad6464691f3359c3
Karine Medrano <kamedran@outlook.com>
2021-04-03 22:10:03 -0500

    changedOverview

--- app.js
+++ app.js
@@ -1,40 +1,46 @@
 $(document).ready(function () {
     ///This runs the click only once
     $("#signBtn").unbind("click").click(function (event) {
-        //event.preventDefault();
+        event.preventDefault();
         let email = $("#inputEmail").val().trim();
         let password = $("#inputPassword").val().trim();
+        let admin = false;
 
         if (email != "" && password != "") {
             //Triggers this call when correct email and password are inputted
-            /* $.getJSON("http://localhost:8080/api/login?username=" + email + "&password=" + password, function (result) {
-            var llave = JSON.parse(data)
-            //response data are now in the result variable
-            alert(llave.token);
-
-            }); */
             $.ajax({
                 url: "http://localhost:8080/api/login?username=" + email + "&password=" + password,
                 type: 'get',
-                //async: false,
-                //dataType: 'json',
                 data: { username: email, password: password },
-                //FIGURE OUT WHAT TO DO WITH DATA
-                /* success: function(response){
-                    alert(response.token);
-                },
-                error: () => {
-                    alert("Wrong Username " + email);
-                } */
-            }).done(function(reponse){
-                //var obj = JSON.parse(data);
-                alert("success");
-            }).fail(function(error ){
-                //alert("Fail" + error);
+            }).done(function (response) {
+                var obj = JSON.stringify(response)
+                console.log(obj)
+                //alert("working");
+                $.ajax({
+                    url: "http://localhost:8080/isAdmin",
+                    type: 'get',
+                    
+                }).done(function (reponse) {
+                    //var obj = JSON.stringify(reponse);
+                    console.log("succesfully in")
+                    //alert("working");
+
+                }).fail(function (error) {
+                    console.log("failure on my part")
+                    //$("#error").removeClass("wrongText");
+                    //window.open("");
+                })
+            }).fail(function (error) {
+                //alert("Fail");
                 $("#error").removeClass("wrongText");
-            }) 
+                //window.open("");
+            })
+
+
         }
     });
+
+
     ///for registration form 
 });
 
@@ -56,8 +62,8 @@ $(document).ready(function () {
                 url: "http://localhost:8080/api/register?username=" + username + "&password=" + password + "&firstName=" + firstName + "&lastName=" + lastName,
                 type: 'post',
                 data: { username: username, password: password, fn: firstName, ln: lastName },
-            
-            }).done(function(){
+
+            }).done(function () {
                 alert("Account Succesfully Registered");
             })
         }
