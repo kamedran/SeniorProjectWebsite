e2c63fbdcff002ca7b2bf87994893ba1cc5a4fe4
Karine Medrano <kamedran@outlook.com>
2021-04-11 23:56:44 -0500

    saved token and student can see info

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var result;
 $(document).ready(function () {
     ///This runs the click only once
     $("#signBtn").unbind("click").click(function (event) {
@@ -11,27 +12,29 @@ $(document).ready(function () {
                 url: "http://localhost:8080/api/login?username=" + email + "&password=" + password,
                 type: 'get',
                 data: { username: email, password: password },
-            }).done(function (response) { 
+            }).done(function (response) {
                 //ajax response to check if the user is an admin or student 
                 isAdmin(response);
-              
+                //saves token globally 
+                localStorage.setItem('token', response.token);
             }).fail(function (error) {
                 //Error message that shows user entered wrong password/email
                 $("#error").removeClass("wrongText");
             })
+            return result;
         }
     });
 });
 
 //Function that checks if user is ADMIN
-function isAdmin(userInfo) {
+function isAdmin(isAdmin) {
     ///This runs the click only once
     $.ajax({
         url: "http://localhost:8080/isAdmin",
         type: 'get',
-        headers: {Authorization: 'Bearer ' + userInfo.token
-    }
-        //'Access-Control-Allow-Origin': '*'}
+        headers: {
+            Authorization: 'Bearer ' + isAdmin.token
+        }
     }).done(function (reponse) {
         //opens the window for admin 
         window.location.assign("Admin/admin_view_student.html")
@@ -42,52 +45,31 @@ function isAdmin(userInfo) {
     })
 };
 
-//Function that shows user information if not ADMIN 
-/* function userOverview(userInfo) {
-    ///This runs the click only once
-    $.ajax({
-        url: "http://localhost:8080/MyInfo",
-        type: 'get',
-        headers: {Authorization: 'Bearer ' + userInfo.token
-    }
-        //'Access-Control-Allow-Origin': '*'}
-    }).done(function (reponse) {
-        //opens the window for admin 
-       console.log("hello user")
-
-    }).fail(function (error) {
-        //opens the window to the student page
-        console.log("goodbye user");
-    })
-}; */
 
 //Function that lets administrator see student information 
-/* function adminInfo(studentInfo) {
-    ///This runs the click only once
+///This runs the click only once
+$(document).ready(function () {
     $("#searchingBtn").unbind("click").click(function (event) {
         event.preventDefault();
         let studentEmail = $("#studentEmail").val().trim();
-        let studentID = $("#studenID").val().trim();
+       // let studentID = $("#studentID").val().trim();
+        $.ajax({
+            url: "http://localhost:8080/userInfo",
+            type: 'get',
+            //headers: { Authorization: 'Bearer ' + result },
+            headers: { "Authorization": 'Bearer ' + localStorage.getItem('token') },
+            data: { email: studentEmail },
+        }).done(function (response) {
+            //ajax response to check if the user is an admin or student 
+            var obj = JSON.stringify(response);
+            console.log(obj);
 
-        if (email != "" && password != "") {
-            //Triggers this call when correct email and password are inputted
-            $.ajax({
-                url: "http://localhost:8080/MyInfo",
-                type: 'get',
-                headers: {Authorization: 'Bearer ' + studentInfo.token},
-                data: { email: studentEmail, userid: studentID},
-            }).done(function (response) { 
-                //ajax response to check if the user is an admin or student 
-                var obj = JSON.stringify(response);
-                console.log(obj);
-              
-            }).fail(function (error) {
-                //Error message that shows user entered wrong password/email
-                console.log("could not load");
-            })
-        }
+        }).fail(function () {
+            //Error message that shows user entered wrong password/email
+            console.log("unable to load")
+        })
     });
-}; */
+});
 
 
 //Registers the user
